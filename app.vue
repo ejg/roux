de46276d586a59bc1f66fbd553af5332734b66bb
ejg <egillcoat@gmail.com>
2022-12-14 15:33:54 -0600

    config

--- app.vue
+++ app.vue
@@ -1,5 +1,16 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="wrapper">
+    <a href="/roux/cmll">CMLL</a>
+    <a href="/roux/pll">PLL</a>
+    <a href="/roux/eo">EO</a>
+    <a href="/roux/cmll/random">Random</a>
   </div>
 </template>
+<style>
+a {
+  display: block;
+  background: #eef;
+  padding: 20px 20px 20px 30px;
+  margin: 5px 0;
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+  css: [
+    // CSS file in the project
+    '@/assets/css/style.css',
+  ],
+  app: {
+    head: {
+      title: 'roux cubing',
+      link: [
+        { rel: 'icon', href: '@/favicon.ico' }
+      ],
+      noscript: [
+        { children: 'Javascript is required' }
+      ]
+    }
+  }
 })
